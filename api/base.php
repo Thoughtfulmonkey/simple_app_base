017a44ffc1ec873cabf02881c935b43206093a6f
Matthew <thoughtfulmonkey@gmail.com>
2026-09-21 09:33:52 +0100

    Index page fixes

--- api/base.php
+++ api/base.php
@@ -6,7 +6,7 @@ require './connection.php';
 require './bumpcheck.php';
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $dbuser, $dbpassword);
 
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
--- api/bumpcheck.php
+++ api/bumpcheck.php
@@ -15,6 +15,4 @@
         exit();
     }
 
-    echo "<p>No issue</p>";
-
 ?>
